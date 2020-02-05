1a285f08668ebacb41792eec3d22f848f0db1c68
Guillermo Salcedo <guillosc@gmail.com>
2020-02-04 19:53:03 -0600

    Updates

---
+++ precache-manifest.356cba79f135607d93762e5bac9c1af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57653c1d7c164fc4432746c3f54d8c08",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "911bc7475e629c37a8c1",
+    "url": "/React-Clicky-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f1ef7a48a76babc07adf",
+    "url": "/React-Clicky-Game/static/css/main.c24e00e6.chunk.css"
+  },
+  {
+    "revision": "911bc7475e629c37a8c1",
+    "url": "/React-Clicky-Game/static/js/2.c5fb446c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/React-Clicky-Game/static/js/2.c5fb446c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ef7a48a76babc07adf",
+    "url": "/React-Clicky-Game/static/js/main.4848f0ff.chunk.js"
+  },
+  {
+    "revision": "b2a275fcf50225a0c022",
+    "url": "/React-Clicky-Game/static/js/runtime-main.2c17f82b.js"
+  }
+]);
